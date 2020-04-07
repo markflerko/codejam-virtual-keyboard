d1e13918e8136fb973dda77759e2d407db7545ae
markflerko <markflerko@gmail.com>
2020-04-07 22:53:12 +0300

    feat: add lang switch ru/en

--- script.js
+++ script.js
@@ -1,10 +1,210 @@
 function searchInKeyboard(code) {
-  for (let i = 0; i<keyboard.length; i++) {
-    if (code==keyboard[i].code) return keyboard[i].key;
+  for (let i = 0; i < keyboard.length; i++) {
+    if (code == keyboard[i].code) return keyboard[i].key;
   }
 }
 
-const keyboard = [
+function swapLanguage(lang) {
+  if (lang == "lang ru") {
+    keyboard = [
+      { key: "`", code: "Backquote" },
+      { key: "1", code: "Digit1" },
+      { key: "2", code: "Digit2" },
+      { key: "3", code: "Digit3" },
+      { key: "4", code: "Digit4" },
+      { key: "5", code: "Digit5" },
+      { key: "6", code: "Digit6" },
+      { key: "7", code: "Digit7" },
+      { key: "8", code: "Digit8" },
+      { key: "9", code: "Digit9" },
+      { key: "0", code: "Digit0" },
+      { key: "-", code: "Minus" },
+      { key: "=", code: "Equal" },
+      { key: "Backspace", code: "Backspace" },
+      { key: "Tab", code: "Tab" },
+      { key: "й", code: "KeyQ" },
+      { key: "ц", code: "KeyW" },
+      { key: "у", code: "KeyE" },
+      { key: "к", code: "KeyR" },
+      { key: "е", code: "KeyT" },
+      { key: "н", code: "KeyY" },
+      { key: "г", code: "KeyU" },
+      { key: "ш", code: "KeyI" },
+      { key: "щ", code: "KeyO" },
+      { key: "з", code: "KeyP" },
+      { key: "х", code: "BracketLeft" },
+      { key: "ъ", code: "BracketRight" },
+      { key: "\\", code: "Backslash" },
+      { key: "CapsLock", code: "CapsLock" },
+      { key: "ф", code: "KeyA" },
+      { key: "ы", code: "KeyS" },
+      { key: "в", code: "KeyD" },
+      { key: "а", code: "KeyF" },
+      { key: "п", code: "KeyG" },
+      { key: "р", code: "KeyH" },
+      { key: "о", code: "KeyJ" },
+      { key: "л", code: "KeyK" },
+      { key: "д", code: "KeyL" },
+      { key: "ж", code: "Semicolon" },
+      { key: "э", code: "Quote" },
+      { key: "Enter", code: "Enter" },
+      { key: "Shift", code: "ShiftLeft" },
+      { key: "я", code: "KeyZ" },
+      { key: "ч", code: "KeyX" },
+      { key: "с", code: "KeyC" },
+      { key: "м", code: "KeyV" },
+      { key: "и", code: "KeyB" },
+      { key: "т", code: "KeyN" },
+      { key: "ь", code: "KeyM" },
+      { key: "б", code: "Comma" },
+      { key: "ю", code: "Period" },
+      { key: "/", code: "Slash" },
+      { key: "▲", code: "ArrowUp" },
+      { key: "Shift", code: "ShiftRight" },
+      { key: "Control", code: "ControlLeft" },
+      { key: "lang en", code: "MetaLeft" },
+      { key: "Alt", code: "AltLeft" },
+      { key: " ", code: "Space" },
+      { key: "Alt", code: "AltRight" },
+      { key: "Control", code: "ControlRight" },
+      { key: "◄", code: "ArrowLeft" },
+      { key: "▼", code: "ArrowDown" },
+      { key: "►", code: "ArrowRight" },
+    ];
+      document.querySelector('#keyboard .key[data="' + keyboard[15].code + '"]').innerHTML = "й";
+      document.querySelector('#keyboard .key[data="' + keyboard[16].code + '"]').innerHTML = "ц";
+      document.querySelector('#keyboard .key[data="' + keyboard[17].code + '"]').innerHTML = "у";
+      document.querySelector('#keyboard .key[data="' + keyboard[18].code + '"]').innerHTML = "к";
+      document.querySelector('#keyboard .key[data="' + keyboard[19].code + '"]').innerHTML = "е";
+      document.querySelector('#keyboard .key[data="' + keyboard[20].code + '"]').innerHTML = "н";
+      document.querySelector('#keyboard .key[data="' + keyboard[21].code + '"]').innerHTML = "г";
+      document.querySelector('#keyboard .key[data="' + keyboard[22].code + '"]').innerHTML = "ш";
+      document.querySelector('#keyboard .key[data="' + keyboard[23].code + '"]').innerHTML = "щ";
+      document.querySelector('#keyboard .key[data="' + keyboard[24].code + '"]').innerHTML = "з";
+      document.querySelector('#keyboard .key[data="' + keyboard[25].code + '"]').innerHTML = "х";
+      document.querySelector('#keyboard .key[data="' + keyboard[26].code + '"]').innerHTML = "ъ";
+      document.querySelector('#keyboard .key[data="' + keyboard[29].code + '"]').innerHTML = "ф";
+      document.querySelector('#keyboard .key[data="' + keyboard[30].code + '"]').innerHTML = "ы";
+      document.querySelector('#keyboard .key[data="' + keyboard[31].code + '"]').innerHTML = "в";
+      document.querySelector('#keyboard .key[data="' + keyboard[32].code + '"]').innerHTML = "а";
+      document.querySelector('#keyboard .key[data="' + keyboard[33].code + '"]').innerHTML = "п";
+      document.querySelector('#keyboard .key[data="' + keyboard[34].code + '"]').innerHTML = "р";
+      document.querySelector('#keyboard .key[data="' + keyboard[35].code + '"]').innerHTML = "о";
+      document.querySelector('#keyboard .key[data="' + keyboard[36].code + '"]').innerHTML = "л";
+      document.querySelector('#keyboard .key[data="' + keyboard[37].code + '"]').innerHTML = "д";
+      document.querySelector('#keyboard .key[data="' + keyboard[38].code + '"]').innerHTML = "ж";
+      document.querySelector('#keyboard .key[data="' + keyboard[39].code + '"]').innerHTML = "э";
+      document.querySelector('#keyboard .key[data="' + keyboard[42].code + '"]').innerHTML = "я";
+      document.querySelector('#keyboard .key[data="' + keyboard[43].code + '"]').innerHTML = "ч";
+      document.querySelector('#keyboard .key[data="' + keyboard[44].code + '"]').innerHTML = "с";
+      document.querySelector('#keyboard .key[data="' + keyboard[45].code + '"]').innerHTML = "м";
+      document.querySelector('#keyboard .key[data="' + keyboard[46].code + '"]').innerHTML = "и";
+      document.querySelector('#keyboard .key[data="' + keyboard[47].code + '"]').innerHTML = "т";
+      document.querySelector('#keyboard .key[data="' + keyboard[48].code + '"]').innerHTML = "ь";
+      document.querySelector('#keyboard .key[data="' + keyboard[49].code + '"]').innerHTML = "б";
+      document.querySelector('#keyboard .key[data="' + keyboard[50].code + '"]').innerHTML = "ю";
+  } else if (lang == "lang en") {
+    keyboard = [
+      { key: "`", code: "Backquote" },
+      { key: "1", code: "Digit1" },
+      { key: "2", code: "Digit2" },
+      { key: "3", code: "Digit3" },
+      { key: "4", code: "Digit4" },
+      { key: "5", code: "Digit5" },
+      { key: "6", code: "Digit6" },
+      { key: "7", code: "Digit7" },
+      { key: "8", code: "Digit8" },
+      { key: "9", code: "Digit9" },
+      { key: "0", code: "Digit0" },
+      { key: "-", code: "Minus" },
+      { key: "=", code: "Equal" },
+      { key: "Backspace", code: "Backspace" },
+      { key: "Tab", code: "Tab" },
+      { key: "q", code: "KeyQ" },
+      { key: "w", code: "KeyW" },
+      { key: "e", code: "KeyE" },
+      { key: "r", code: "KeyR" },
+      { key: "t", code: "KeyT" },
+      { key: "y", code: "KeyY" },
+      { key: "u", code: "KeyU" },
+      { key: "i", code: "KeyI" },
+      { key: "o", code: "KeyO" },
+      { key: "p", code: "KeyP" },
+      { key: "[", code: "BracketLeft" },
+      { key: "]", code: "BracketRight" },
+      { key: "\\", code: "Backslash" },
+      { key: "CapsLock", code: "CapsLock" },
+      { key: "a", code: "KeyA" },
+      { key: "s", code: "KeyS" },
+      { key: "d", code: "KeyD" },
+      { key: "f", code: "KeyF" },
+      { key: "g", code: "KeyG" },
+      { key: "h", code: "KeyH" },
+      { key: "j", code: "KeyJ" },
+      { key: "k", code: "KeyK" },
+      { key: "l", code: "KeyL" },
+      { key: ";", code: "Semicolon" },
+      { key: "'", code: "Quote" },
+      { key: "Enter", code: "Enter" },
+      { key: "Shift", code: "ShiftLeft" },
+      { key: "z", code: "KeyZ" },
+      { key: "x", code: "KeyX" },
+      { key: "c", code: "KeyC" },
+      { key: "v", code: "KeyV" },
+      { key: "b", code: "KeyB" },
+      { key: "n", code: "KeyN" },
+      { key: "m", code: "KeyM" },
+      { key: ",", code: "Comma" },
+      { key: ".", code: "Period" },
+      { key: "/", code: "Slash" },
+      { key: "▲", code: "ArrowUp" },
+      { key: "Shift", code: "ShiftRight" },
+      { key: "Control", code: "ControlLeft" },
+      { key: "lang ru", code: "MetaLeft" },
+      { key: "Alt", code: "AltLeft" },
+      { key: " ", code: "Space" },
+      { key: "Alt", code: "AltRight" },
+      { key: "Control", code: "ControlRight" },
+      { key: "◄", code: "ArrowLeft" },
+      { key: "▼", code: "ArrowDown" },
+      { key: "►", code: "ArrowRight" },
+    ];
+    document.querySelector('#keyboard .key[data="' + keyboard[15].code + '"]').innerHTML = "q";
+    document.querySelector('#keyboard .key[data="' + keyboard[16].code + '"]').innerHTML = "w";
+    document.querySelector('#keyboard .key[data="' + keyboard[17].code + '"]').innerHTML = "e";
+    document.querySelector('#keyboard .key[data="' + keyboard[18].code + '"]').innerHTML = "r";
+    document.querySelector('#keyboard .key[data="' + keyboard[19].code + '"]').innerHTML = "t";
+    document.querySelector('#keyboard .key[data="' + keyboard[20].code + '"]').innerHTML = "y";
+    document.querySelector('#keyboard .key[data="' + keyboard[21].code + '"]').innerHTML = "u";
+    document.querySelector('#keyboard .key[data="' + keyboard[22].code + '"]').innerHTML = "i";
+    document.querySelector('#keyboard .key[data="' + keyboard[23].code + '"]').innerHTML = "o";
+    document.querySelector('#keyboard .key[data="' + keyboard[24].code + '"]').innerHTML = "p";
+    document.querySelector('#keyboard .key[data="' + keyboard[25].code + '"]').innerHTML = "[";
+    document.querySelector('#keyboard .key[data="' + keyboard[26].code + '"]').innerHTML = "]";
+    document.querySelector('#keyboard .key[data="' + keyboard[29].code + '"]').innerHTML = "a";
+    document.querySelector('#keyboard .key[data="' + keyboard[30].code + '"]').innerHTML = "s";
+    document.querySelector('#keyboard .key[data="' + keyboard[31].code + '"]').innerHTML = "d";
+    document.querySelector('#keyboard .key[data="' + keyboard[32].code + '"]').innerHTML = "f";
+    document.querySelector('#keyboard .key[data="' + keyboard[33].code + '"]').innerHTML = "g";
+    document.querySelector('#keyboard .key[data="' + keyboard[34].code + '"]').innerHTML = "h";
+    document.querySelector('#keyboard .key[data="' + keyboard[35].code + '"]').innerHTML = "j";
+    document.querySelector('#keyboard .key[data="' + keyboard[36].code + '"]').innerHTML = "k";
+    document.querySelector('#keyboard .key[data="' + keyboard[37].code + '"]').innerHTML = "l";
+    document.querySelector('#keyboard .key[data="' + keyboard[38].code + '"]').innerHTML = ";";
+    document.querySelector('#keyboard .key[data="' + keyboard[39].code + '"]').innerHTML = "'";
+    document.querySelector('#keyboard .key[data="' + keyboard[42].code + '"]').innerHTML = "z";
+    document.querySelector('#keyboard .key[data="' + keyboard[43].code + '"]').innerHTML = "x";
+    document.querySelector('#keyboard .key[data="' + keyboard[44].code + '"]').innerHTML = "c";
+    document.querySelector('#keyboard .key[data="' + keyboard[45].code + '"]').innerHTML = "v";
+    document.querySelector('#keyboard .key[data="' + keyboard[46].code + '"]').innerHTML = "b";
+    document.querySelector('#keyboard .key[data="' + keyboard[47].code + '"]').innerHTML = "n";
+    document.querySelector('#keyboard .key[data="' + keyboard[48].code + '"]').innerHTML = "m";
+    document.querySelector('#keyboard .key[data="' + keyboard[49].code + '"]').innerHTML = ",";
+    document.querySelector('#keyboard .key[data="' + keyboard[50].code + '"]').innerHTML = ".";
+  }
+}
+
+let keyboard = [
   { key: "`", code: "Backquote" },
   { key: "1", code: "Digit1" },
   { key: "2", code: "Digit2" },
@@ -57,17 +257,17 @@ const keyboard = [
   { key: ",", code: "Comma" },
   { key: ".", code: "Period" },
   { key: "/", code: "Slash" },
-  { key: "ArrowUp", code: "ArrowUp" },
+  { key: "▲", code: "ArrowUp" },
   { key: "Shift", code: "ShiftRight" },
   { key: "Control", code: "ControlLeft" },
-  { key: "Meta", code: "MetaLeft" },
+  { key: "lang ru", code: "MetaLeft" },
   { key: "Alt", code: "AltLeft" },
   { key: " ", code: "Space" },
   { key: "Alt", code: "AltRight" },
   { key: "Control", code: "ControlRight" },
-  { key: "ArrowLeft", code: "ArrowLeft" },
-  { key: "ArrowDown", code: "ArrowDown" },
-  { key: "ArrowRight", code: "ArrowRight" },
+  { key: "◄", code: "ArrowLeft" },
+  { key: "▼", code: "ArrowDown" },
+  { key: "►", code: "ArrowRight" },
 ];
 
 function init() {
@@ -90,23 +290,43 @@ function init() {
 init();
 
 // keydown
-document.addEventListener("keydown", function (event) {
-  document.querySelector('#textarea').innerHTML += event.key;
-  document.querySelectorAll('#keyboard .key').forEach(function(element) {
+document.addEventListener("keydown", (event) => {
+  event.preventDefault();
+  if (event.code == "Backspace") {
+    let str = document.querySelector('#textarea').innerHTML;
+    document.querySelector('#textarea').innerHTML = str.slice(0, -1);
+  } else if (event.code == "MetaLeft") {
+    event.preventDefault();
+    let key = searchInKeyboard("MetaLeft");
+    swapLanguage(key);
+  } else {
+    let key = searchInKeyboard(event.code);
+    document.querySelector('#textarea').innerHTML += key;
+  }
+  document.querySelectorAll('#keyboard .key').forEach(function (element) {
     element.classList.remove('active');
   });
-  document.querySelector('#keyboard .key[data="' + event.code + '"]').classList.add('active');   
+  document.querySelector('#keyboard .key[data="' + event.code + '"]').classList.add('active');
 });
 
 // onclick
-document.querySelectorAll('#keyboard .key').forEach(function(element) {
-  element.onclick = function(event) {
-    document.querySelectorAll('#keyboard .key').forEach(function(element) {
+document.querySelectorAll('#keyboard .key').forEach((element) => {
+  element.onclick = function (event) {
+    document.querySelectorAll('#keyboard .key').forEach(function (element) {
       element.classList.remove('active');
     });
     let code = this.getAttribute('data');
     this.classList.add("active");
-    document.querySelector('#textarea').innerHTML += searchInKeyboard(code);
+    let key = searchInKeyboard(code);
+
+
+
+    if (key == "Backspace") {
+      let str = document.querySelector('#textarea').innerHTML;
+      document.querySelector('#textarea').innerHTML = str.slice(0, -1);
+    } else {
+      document.querySelector('#textarea').innerHTML += key;
+    }
   }
 });
 
